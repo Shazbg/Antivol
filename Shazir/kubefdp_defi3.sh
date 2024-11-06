97ece3281f2ebab6bfdc96c1eda6a250d5312d0c
Shazir Sheik <shazir.sheik@telecom-sudparis.eu>
2024-11-06 16:19:00 +0100

    oui

--- Shazir/kubefdp_defi3.sh
+++ Shazir/kubefdp_defi3.sh
@@ -1,13 +1,14 @@
 #/bin/zsh
 
-# kubectl delete deployment web-vroumvroum-deployment
-# kubectl delete deployment db-vroumvroum-deployment
-# kubectl delete svc db-vroumvroum-svc
-# kubectl delete svc web-vroumvroum-svc
+kubectl delete deployment web-vroumvroum-deployment -n u-mssxs
+kubectl delete deployment db-vroumvroum-deployment -n u-mssxs
+kubectl delete svc db-vroumvroum-svc -n u-mssxs
+kubectl delete svc web-vroumvroum-svc -n u-mssxs
+kubectl delete ingress web-vroumvroum-ingress -n u-mssxs
 
-kubectl apply -f db-vroumvroum-svc.yaml 
-kubectl apply -f db-vroumvroum-deployment.yaml
+kubectl apply -f db-vroumvroum-svc.yaml -n u-mssxs
+kubectl apply -f db-vroumvroum-deployment.yaml -n u-mssxs
 sleep 5 # wait for db svc to be ready
-kubectl apply -f web-vroumvroum-svc.yaml      
-kubectl apply -f web-vroumvroum-deployment.yaml
-kubectl apply -f web-vroumvroum-ingress.yaml
+kubectl apply -f web-vroumvroum-svc.yaml -n u-mssxs      
+kubectl apply -f web-vroumvroum-deployment.yaml -n u-mssxs
+kubectl apply -f web-vroumvroum-ingress.yaml -n u-mssxs
